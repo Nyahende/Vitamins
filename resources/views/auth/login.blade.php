751d0b386922f7497ad003cb6bd44a346b357af6
Nyahende <michaelnyahende8@gmail.com>
2022-07-20 02:35:06 -0700

    custom_php.ini

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>ST.THOMAS CLINIC | Login</title>
+        <title>VITAMINS | Login</title>
 
         <!-- Google Font: Source Sans Pro -->
         <link
